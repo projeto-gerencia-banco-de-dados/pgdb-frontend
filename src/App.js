2298e8f56559078e504e8d90dd6ad1c1ea4f7499
Francisco <franciscoctism@gmail.com>
2023-01-20 10:23:35 -0300

    sign in and sign up screens

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SignIn from './pages/sign-in/sign-in';
+import SignUp from './pages/sign-up/sign-up';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={SignIn} />
+                <Route exact path="/login" component={SignIn} />
+                <Route exact path="/sign-up" component={SignUp} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
